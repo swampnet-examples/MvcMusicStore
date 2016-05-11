c8203100c60884d6f09dc5b56a1430d0177e0c34
Pete Whitby <petejw@theswamp.co.uk>
2016-05-11 20:28:48 +0100

    Add project files.

---
+++ MvcMusicStore/Models/MusicStoreDBInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcMusicStore.Models
+{
+	public class MusicStoreDBInitializer : System.Data.Entity.DropCreateDatabaseAlways<MusicStoreDB>
+	{
+		protected override void Seed(MusicStoreDB context)
+		{
+			context.Artists.Add(new Artist() { Name = "Al Di Meola" });
+			context.Genres.Add(new Genre() { Name = "Jazz" });
+			context.Albums.Add(new Album()
+			{
+				 Artist = new Artist() { Name = "Rush"},
+				 Genre = new Genre() { Name = "Rock" },
+				 Price = 9.99M,
+				 Title = "Caravan"
+			});
+
+			base.Seed(context);
+		}
+	}
+}
